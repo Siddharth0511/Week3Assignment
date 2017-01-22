2169d55195d0fdf110de4c2782e505aeabbfd8d0
Siddharth0511 <siddharth.khandelwal0511@gmail.com>
2017-01-22 16:51:13 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,14 @@
 
 makeCacheMatrix <- function(x = matrix()) 
 {
-  m<-matrix(,nrow=nrow(x),ncol=ncol(x))
+  m<-matrix(,nrow=nrow(x),ncol=ncol(x)) #Declaring a null matrix
   set <-function(y)
   {
   x<<-y
-  m<<-matrix(,nrow=nrow(x),ncol=ncol(x))
+  m<<-matrix(,nrow=nrow(x),ncol=ncol(x)) #Declaring a null matrix when x is reset
   }
   get<-function() x
-  setinverse<-function(inverse) m<<-inverse
+  setinverse<-function(inverse) m<<-inverse # Setting m to the inverse
   getinverse<-function() m
   list(set=set, get=get, setinverse=setinverse,getinverse=getinverse)
 }
@@ -19,14 +19,14 @@ makeCacheMatrix <- function(x = matrix())
 
 cacheSolve <- function(x, ...) 
 {
-  m<-x$getinverse()
-  if(!all(is.na(m)))
+  m<-x$getinverse() #Get inverse from cache
+  if(!all(is.na(m))) # Check if all elements are not null so that when not null, cached data is returned
     {
     message("getting cached data")
     return(m)
     }
-  data<-x$get()
-  m<-solve(data)
-  x$setinverse(m)
-  m
+  data<-x$get() #get the matrix
+  m<-solve(data) # calculate inverse assuming invertible square matrix as mentioned in the question
+  x$setinverse(m) # set m with the inverse
+  m # REturn inverse matrix
 }
